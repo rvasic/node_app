711611034be88e9f8d265efc6fd2412985ea7901
rvasic <rajko.vasic1997@gmail.com>
2022-03-13 16:01:10 +0100

    Port 5555

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ app.get('/', function (req, res) {
    res.send('Zdravo svete!');
 })
 
-var server = app.listen(3000, function () {
-   var host = server.address().address
+var server = app.listen(5555, function () {
    var port = server.address().port
    
-   console.log("Primer apliakcije koja slusa na http://%s:%s", host, port)
+   console.log("Primer apliakcije koja slusa na portu %s", port)
 })
