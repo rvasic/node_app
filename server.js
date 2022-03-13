00b175ab4b1e47556cdb50c9b48b8106a56d4233
rvasic <rajko.vasic1997@gmail.com>
2022-03-13 16:05:26 +0100

    New App

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-var express = require('express');
-var app = express();
+// Load HTTP module
+const http = require("http");
 
-app.get('/', function (req, res) {
-   res.send('Zdravo svete!');
-})
+const hostname = "127.0.0.1";
+const port = 3000;
 
-var server = app.listen(5555, function () {
-   var port = server.address().port
-   
-   console.log("Primer apliakcije koja slusa na portu %s", port)
+// Create HTTP server
+const server = http.createServer(function(req, res) {
+
+   // Set the response HTTP header with HTTP status and Content type
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+
+   // Send the response body "Hello World"
+   res.end('Hello World\n');
+});
+
+// Prints a log once the server starts listening
+server.listen(port, hostname, function() {
+   console.log(`Server running at http://${hostname}:${port}/`);
 })
