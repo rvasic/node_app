70da1ff75e112ebedc3b7a67afe7b350cae0a96f
rvasic <rajko.vasic1997@gmail.com>
2022-03-13 15:49:24 +0100

    Edited server.js file again

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const express = require("express");
-const app = express();
+var express = require('express');
+var app = express();
 
-//Definisanje porta
-const port=3000;
+app.get('/', function (req, res) {
+   res.send('Zdravo svete!');
+})
 
-app.get("/", function (req, res) {
-    res.send('Zdravo svete!');
-  })
-
-//Pokretanje aplikacije
-app.listen(port, () => {
-  console.log(`Primer aplikacije koja slusa na portu ${port}`);
-});
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Primer apliakcije koja slusa na http://%s:%s", host, port)
+})
