b1696f3083c7846f54348a90cc056882da372c0a
rvasic <rajko.vasic1997@gmail.com>
2022-03-13 16:28:20 +0100

    Novi ispis express aplikacije

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
-// Ucitavanje HTTP modula
-const http = require("http");
-
-const hostname = "127.0.0.1";
+const express = require('express');
+const app = express();
 const port = 3000;
 
-// Kreiranje HTTP servera
-const server = http.createServer(function(req, res) {
-
-   // Postavljanje odgovora HTTP zaglavlja da sadrzi HTTP status i tip sadrzaja 
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-
-   // Slanje odgovora u body sekciji
-   res.end('Zdravo svete!\n');
+app.get('/', function(req, res) {
+  res.send('ZdraVO svete!')
 });
 
-// Ispisivanje loga u momentu pokretanja servera
-server.listen(port, hostname, function() {
-   console.log(`Server pokrenut na http://${hostname}:${port}/`);
-})
+app.listen(port, function() {
+  console.log(`Aplikacija pokrenuta na portu ${port}!`)
+});
